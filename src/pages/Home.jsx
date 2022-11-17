975807cf495cc17096a3af7ecbaa0bc45c652ae3
willianglitch <nailliw901@gmail.com>
2022-11-16 21:43:00 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "../components/navBar";
+
+export const Home = () => {
+    return(
+        <div>
+            <NavBar />
+        </div>
+    )
+}
